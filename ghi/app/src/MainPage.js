9af6e7506ab0126803b514cadfa0b164bfa6dc0b
daniel-san17 <daniel.rotstein17@gmail.com>
2022-09-10 21:42:18 -0400

    finished project

--- ghi/app/src/MainPage.js
+++ ghi/app/src/MainPage.js
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom';
+
+
 function MainPage() {
   return (
     <div className="px-4 py-5 my-5 text-center">
@@ -8,6 +11,25 @@ function MainPage() {
           the solution for you!
         </p>
       </div>
+
+      <div>
+        
+      </div>
+      <div className="row justify-content-evenly">
+        <div className="col-6 col-sm-4 shadow p-3 mb-5 bg-dark rounded">
+          <Link to="/shoes">
+            <img src="/shoes.jpg" className="img-fluid" alt="shoesImage"/>
+            
+          </Link>
+        </div>
+        <div className="col-6 col-sm-4 shadow p-3 mb-5  bg-dark rounded">
+          <Link to="/hats">
+            <img src="/hats.png" className="img-fluid" alt="hatsImage" />
+          </Link>
+        </div>
+      </div>
+
+
     </div>
   );
 }
