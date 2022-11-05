2fd60f3679105aecce4e7b20d3560f1ec21fb5bb
daniel-san17 <daniel.rotstein17@gmail.com>
2022-11-05 09:36:32 -0400

    cleaned code in hats/poller.py

--- hats/poll/poller.py
+++ hats/poll/poller.py
@@ -12,22 +12,20 @@ django.setup()
 # Import models from hats_rest, here.
 # from hats_rest.models import Something
 
-
 from hats_rest.models import LocationVO
 
 
 def get_locations():
     response = requests.get("http://wardrobe-api:8000/api/locations/")
     content = json.loads(response.content)
-    print("---VISCA BARÇA---")
+    print("---Hats---")
     for location in content["locations"]:
-        LocationVO.objects.update_or_create( 
+        LocationVO.objects.update_or_create(
             import_href=location["href"],
             defaults={
                 "closet_name": location["closet_name"],
             }
         )
-
 
 
 def poll():
