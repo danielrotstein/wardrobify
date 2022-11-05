97f4e772e6256b1301df09afde7441c70b39f3b7
daniel-san17 <daniel.rotstein17@gmail.com>
2022-11-05 09:39:42 -0400

    cleaned code in shoes/poller.py

--- shoes/poll/poller.py
+++ shoes/poll/poller.py
@@ -9,7 +9,8 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shoes_project.settings")
 django.setup()
 
-# Import models from hats_rest, here.
+# Import models from shoes_rest, here.
+# from shoes_rest.models import Something
 
 from shoes_rest.models import BinVO
 
@@ -17,7 +18,7 @@ from shoes_rest.models import BinVO
 def get_bins():
     response = requests.get("http://wardrobe-api:8000/api/bins/")
     content = json.loads(response.content)
-    print(" <<< HOPP YB >>> ")
+    print("<<<Shoes>>>")
     for bin in content["bins"]:
         BinVO.objects.update_or_create(
             import_href=bin["href"],
@@ -25,7 +26,6 @@ def get_bins():
                 "closet_name": bin["closet_name"],
             }
         )
-
 
 
 def poll():
