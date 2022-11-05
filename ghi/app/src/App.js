5191f39d61d378cd4a9a4e8a77c49efeb165b0a8
daniel-san17 <daniel.rotstein17@gmail.com>
2022-11-05 09:32:34 -0400

    cleaned code in App.js

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,46 +1,35 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainPage from './MainPage';
+import Nav from './Nav';
+import Footer from './Footer';
+import ShoesForm from './ShoesForm';
+import ShoesList from './ShoesList';
 import HatsList from './HatsList';
 import HatsFrom from './HatsForm';
 
-import Footer from './Footer';
-
-import MainPage from './MainPage';
-import Nav from './Nav';
-
-
-import ShoesForm from './ShoesForm';
-import ShoesList from './ShoesList';
-
 
 function App(props) {
-
-
-if (props.shoes === undefined) {
+if (props.shoes === undefined || props.hats === undefined) {
   return null;
 }
-
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-
           <Route path="shoes">
             <Route index element={<ShoesList shoes={props.shoes}/>} />
             <Route path="new" element={<ShoesForm /> } />
           </Route>
-
           <Route path="hats">
             <Route index element={<HatsList hats={props.hats}/>} />
             <Route path="new" element={<HatsFrom /> } />
           </Route>          
-
         </Routes>
       </div>
       <Footer/>
     </BrowserRouter>
-    
   );
 }
 
