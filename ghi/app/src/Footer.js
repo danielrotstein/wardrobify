9a76cf7c2f54eddba0acc9229c3877ef091f0d3c
daniel-san17 <daniel.rotstein17@gmail.com>
2022-11-05 09:33:27 -0400

    cleaned code in Footer.js

--- ghi/app/src/Footer.js
+++ ghi/app/src/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const Footer = () => (
   <div className="footer bg-warning fixed-bottom max-width p-2">
     <p className="text-lg-center">Copyright by Daniel Rotstein | 2022</p>
